621d06c781ecb264990407b43b61c9cc5c202ed6
Kiran jadhav <kiranjadhav9561@gmail.com>
2023-04-25 19:17:36 +0530

    3rd page completed

--- src/componets/page2/booking/Booking.jsx
+++ src/componets/page2/booking/Booking.jsx
@@ -3,7 +3,117 @@ import "./Booking.css";
 const Booking = () => {
   return (
     <div className="priceAndBooking">
-      <div className="bookingSummary">booking</div>
+      <div className="bookingSummary">
+        <div className="bookingSumm">
+          <div className="bookingTitle">Booking summary</div>
+          <div className="express">
+            <div>Express</div>
+            <img
+              width={28.52}
+              height={23}
+              src="https://i.ibb.co/V3fxj2b/Plane-icon.png"
+              alt="Plane-icon"
+              border="0"
+            />
+          </div>
+          <div className="DelhiShanghai">
+            <div className="delhi">
+              {" "}
+              <img
+                width={35}
+                height={24.5}
+                src="https://i.ibb.co/rt2SGNH/icon.png"
+                alt="icon"
+                border="0"
+              />
+            </div>
+
+            <hr className="DelhiShanghaiLine" />
+            <div className="shanghai">
+              <img
+                className="shanghai"
+                width={35}
+                height={24.5}
+                src="https://i.ibb.co/rt2SGNH/icon.png"
+                alt="icon"
+                border="0"
+              />
+            </div>
+          </div>
+          <div className="destination">
+            <div className="delhi">
+              <span>Delhi,</span>
+              <span>110003 India</span>
+            </div>
+            <div className="shanghai">
+              <span>Delhi,</span>
+              <span>110003 India</span>
+            </div>
+          </div>
+        </div>
+        <div className="weightAndLoad">
+          <div className="weightBooking">
+            <div className="titleweight">Total Weight/Volume</div>
+            <div className="weightImage">
+              <img
+                width={65}
+                height={65}
+                src="https://i.ibb.co/D8SW6jW/weightmeter.png"
+                alt="weightmeter"
+                border="0"
+              />
+            </div>
+            <div className="weightValue">114.21KG</div>
+          </div>
+          <div className="loadBookig">
+            <div className="loadTitle">Load</div>
+
+            <div className="imageContainer">
+              <div className="bookingScale">1 X</div>
+              <div className="loadImage">
+                <img
+                  width={46}
+                  height={44}
+                  src="https://i.ibb.co/9g8TwQs/Vector-3.png"
+                  alt="Vector-3"
+                  border="0"
+                />
+                <div className="imageTitle">Pallets</div>
+              </div>
+            </div>
+            <div className="LoadimageDesc">230 X 140 X 120 CM</div>
+          </div>
+        </div>
+        <div className="LastRow">
+          <div className="SellerPrimetimeWorldwide">
+            <div className="TitlePrimetimeWorldwide">
+              Seller: Primetime Worldwide
+            </div>
+            <div className="ImagePrimetimeWorldwide">
+              <img
+                width={141}
+                height={45}
+                src="https://i.ibb.co/tKHVm3z/Frame-64.png"
+                alt="Frame-64"
+                border="0"
+              />
+            </div>
+          </div>
+          <div className="InsuranceLastRow">
+            <div className="TitleInsuranceLastRow">Insurance:Xcover.com</div>
+            <div className="ImageInsuranceLastRow">
+              <img
+                width={219}
+                height={34}
+                src="https://i.ibb.co/Hp6fvVZ/Xcover.png"
+                alt="Xcover"
+                border="0"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* 🟡Price Section🟡 */}
       <div className="priceDetails">
         <div className="TitleAndPrice">
@@ -31,13 +141,15 @@ const Booking = () => {
         <div className="Insurance">
           <span>Insurance</span>
           <span className="InsuranceSpan">$323.40</span>
+          <div className="BasedOncost">Based on the items cost</div>
         </div>
-        <div className="BasedOncost">Based on the items cost</div>
-        <hr className="BasedOncostUnderLine " />
-        <div className="promoCode">
-          Add a <span> promo code</span>
-        </div>
+
+        <hr className="priceDtsUnderline " />
+
         <div className="Platformfee">
+          <div className="promoCode">
+            Add a <span> promo code</span>
+          </div>
           <span>Platform fee</span>
           <span className="PlatformfeeSpan">$119.48</span>
         </div>
